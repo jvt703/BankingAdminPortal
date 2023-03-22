53a7c97cc80713f80e8a0b5ad96ccfc7a5f38eee
Richard <richardfolorunso@protonmail.com>
2023-03-22 19:38:52 -0400

    Routing fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,15 +18,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoanInfoModalComponent } from './loan-info-modal/loan-info-modal.component';
 import { MatDialogModule} from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select'; 
+import { MatSelectModule } from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator'; 
+import {MatPaginatorModule} from '@angular/material/paginator';
 import { FormsModule } from '@angular/forms';
 import {MatSortModule} from '@angular/material/sort';
-import {MatMenuModule} from '@angular/material/menu'; 
+import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { UserCreateComponent } from './Users/user-create/user-create.component';
+import { UserHomeComponent } from './Users/user-home.component';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -38,13 +45,15 @@ import {MatButtonModule} from '@angular/material/button';
     HomeCardsComponent,
     LoansComponent,
     LoansCardComponent,
-    LoanInfoModalComponent
+    LoanInfoModalComponent,
+    UserHomeComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     MatSnackBarModule,
     MatIconModule,
     ReactiveFormsModule,
@@ -60,7 +69,12 @@ import {MatButtonModule} from '@angular/material/button';
     FormsModule,
     MatSortModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatNativeDateModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatCardModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
