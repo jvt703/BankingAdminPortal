b9113f266804527b841be27066d053ac0b3abefb
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-27 13:33:53 -0500

    Revise service, add updateCreditCardType method

--- src/app/Services/credit-card-type.service.ts
+++ src/app/Services/credit-card-type.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import CreditCardType from '../Interfaces/creditCardType.model';
 import { environment } from 'src/environments/environment.development';
+import { CreditCardType, OutgoingCreditCardTypeDto } from '../Interfaces/creditCardType.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreditCardTypeService {
+  private apiUrl = environment.apiBaseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  createCreditCardType(creditCardType: CreditCardType): Observable<CreditCardType> {
-    return this.http.post<CreditCardType>(environment.apiBaseUrl + '/creditCardType', creditCardType);
+  createCreditCardType(data: OutgoingCreditCardTypeDto): Observable<CreditCardType> {
+    return this.http.post<CreditCardType>(`${this.apiUrl}/creditCardType`, data);
+  }
+
+  getCreditCardTypes(): Observable<CreditCardType[]> {
+    return this.http.get<CreditCardType[]>(`${this.apiUrl}/creditCardTypes`);
+  }
+
+  updateCreditCardType(id: number, data: OutgoingCreditCardTypeDto): Observable<CreditCardType> {
+    return this.http.put<CreditCardType>(`${this.apiUrl}/creditCardType/${id}`, data);
   }
 }
