52046acaac590e686b773489e9e85ed78533b25a
Blaze Vincent <blaze.vincent@smoothstack.com>
2023-03-27 13:32:41 -0500

    Add outgoingCreditCardTypeDto for credit card type edits

--- src/app/Interfaces/creditCardType.model.ts
+++ src/app/Interfaces/creditCardType.model.ts
@@ -1,4 +1,12 @@
-export default interface CreditCardType {
+export interface CreditCardType {
+  id: number;
+  rewardsName: string;
+  interestRate: number;
+  minimumPayment: number;
+  creditLimit: number;
+}
+
+export interface OutgoingCreditCardTypeDto {
   rewardsName: string;
   interestRate: number;
   minimumPayment: number;
